ed42fb45de12fe055d93a5576aee1027d3bdcb6c
Israel Anthony Lagunzad <isanclagunzad@gmail.com>
2024-04-21 03:54:19 +0800

    🎉 initial setup

---
+++ src/lib/config.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+export const currentMode = Number(process.env.NEXT_PUBLIC_ENV) || 0;
+export const config = [
+  // dev mode
+  {
+    api_url: 'https://example.com/api',
+  },
+  // prod mode
+  {
+    api_url: 'https://example.com/api',
+  },
+];
+
+export default {
+  project: 'kita-ph-boilerplate',
+  setting: config[currentMode],
+};
